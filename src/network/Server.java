a616843ba26bd4a60b246023bf8411ff1db89295
Texe1 <73749424+Texe1@users.noreply.github.com>
2021-11-20 21:40:25 +0100

    Client-Server communication changes
    most communication is in ServerThread now.
    Server only accepts and names clients

--- src/network/Server.java
+++ src/network/Server.java
@@ -1,20 +1,24 @@
 package network;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.*;
-
-import aes.Decryption;
-import aes.KeySchedule;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 
 public class Server {
 	
 	public static ServerSocket server;
 	
+	public static HashMap<String, Socket> clients = new HashMap<String, Socket>();
+	
+	public static int clientCounter = 0;
+	
 	public static void start(int port) {
 		try {
 			server = new ServerSocket(port);
-			server.setSoTimeout(100000);
+			server.setSoTimeout(300000);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -25,20 +29,34 @@ public class Server {
 		while(true) {
 			try {
 				Socket client = server.accept();
-				System.out.println("Input detected...\nDecrypting...");
+//				System.out.println("Input detected...\nDecrypting...");
 				DataInputStream in = new DataInputStream(client.getInputStream());
 				String s = in.readUTF();
-				char[] key = s.substring(0, 8).toCharArray();
-				s = s.substring(8);
-				String binStrKey = "";
-				for (char c : key) {
-					String bits = Integer.toBinaryString(c);
-					while (bits.length() < 16) bits = "0" + bits;
-					binStrKey += bits;
+				
+				new ServerThread(s, client).start();
+				
+				if(clients.containsKey(s)) {
+					while(!clients.containsKey(Integer.toString(++clientCounter))) {
+						clients.put(Integer.toString(clientCounter), client);
+					}
+					new DataOutputStream(client.getOutputStream()).writeUTF("n" + clientCounter);
+				}else {
+					clients.put(s, client);
+					new DataOutputStream(client.getOutputStream()).writeUTF("n" + s);
 				}
 				
-				String msg = Decryption.decrypt(s, binStrKey);
-				System.out.println("Decrypted messsage:\n" + msg);
+//				System.out.println(s);
+//				char[] key = s.substring(0, 8).toCharArray();
+//				s = s.substring(8);
+//				String binStrKey = "";
+//				for (char c : key) {
+//					String bits = Integer.toBinaryString(c);
+//					while (bits.length() < 16) bits = "0" + bits;
+//					binStrKey += bits;
+//				}
+//				
+//				String msg = Decryption.decrypt(s, binStrKey);
+//				System.out.println("Decrypted messsage:\n" + msg);
 			} catch (Exception e) {
 				e.printStackTrace();
 				break;
