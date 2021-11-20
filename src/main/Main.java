2a955ef823b5594921065083f6236e6f4ae7fe76
Texe1 <73749424+Texe1@users.noreply.github.com>
2021-11-20 21:44:40 +0100

    Encrypting not needed in this class
    En- and Decryption will be implemented in the Client class later on

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,49 +19,64 @@ public class Main {
 			Server.start(1337);
 			Server.laufen();
 		} else if (in.equals("c")) {
-			String s = "Hallo, Welt";
-
-			// encryption
-			String encrypted = Encryption.encrypt(s);
-			System.out.println(encrypted);
-
-			// binaryString to char[] conversion
-			char[] keyAschars = new char[8];
-
-			String[] splitKey = KeySchedule.Key.split("(?<=\\G.{16})");
-
-			for (int i = 0; i < 8; i++) {
-				keyAschars[i] = (char) Integer.parseInt(splitKey[i], 2);
-				for (int j = 0; j < 16; j++) {
-					System.out.print(((keyAschars[i] & (1 << (15 - j))) != 0) ? "1" : "0");
-				}
+//			String s = "Hallo, Welt";
+//
+//			// encryption
+//			String encrypted = Encryption.encrypt(s);
+////			System.out.println(encrypted);
+//
+//			// binaryString to char[] conversion
+//			char[] keyAschars = new char[8];
+//
+//			String[] splitKey = KeySchedule.Key.split("(?<=\\G.{16})");
+//
+//			for (int i = 0; i < 8; i++) {
+//				keyAschars[i] = (char) Integer.parseInt(splitKey[i], 2);
+//				for (int j = 0; j < 16; j++) {
+////					System.out.print(((keyAschars[i] & (1 << (15 - j))) != 0) ? "1" : "0");
+//				}
+//			}
+//			
+////			System.out.println(Decryption.decrypt(encrypted, KeySchedule.Key));
+//			
+//			String send = "m-eaes";
+//			
+//			for(char c : keyAschars) {
+//				send += "" + c;
+//			}
+//			
+////			System.out.println(send.length());
+//			
+//			send += encrypted;
+//			
+////			System.out.println(send.length());
+////			
+////			
+////			System.out.println();
+////
+////			System.out.println();
+////			for(char c : send.toCharArray()) {
+////				System.out.println((int)c);
+////			}
+////			System.out.println();
+////			System.out.println();
+			
+			Client c = new Client();
+			c.registerToServer("localhost", 1337, "0");
+			c.waitForMessage();
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+			
+//			c.send("Hello, World!");
+			
+			while(true) {
+				;
 			}
 			
-			System.out.println(Decryption.decrypt(encrypted, KeySchedule.Key));
-			
-			String send = "";
-			
-			for(char c : keyAschars) {
-				send += "" + c;
-			}
-			
-			System.out.println(send.length());
-			
-			send += encrypted;
-			
-			System.out.println(send.length());
-			
-			
-			System.out.println();
-
-			System.out.println();
-			for(char c : send.toCharArray()) {
-				System.out.println((int)c);
-			}
-			System.out.println();
-			System.out.println();
-			
-			Client.send(send, "localhost", 1337);
+//			c.send(send, "localhost", 1337);
 		}else {
 			System.out.println("that was not one of the options!");
 		}
