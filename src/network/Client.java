9c2493be9483d8b8fe004e92406d07aa2a9dffdf
Texe1 <73749424+Texe1@users.noreply.github.com>
2021-11-23 21:39:53 +0100

    simple implementation of chats
    chats as a List os Strings, beginning with name and encryption, then Following the messages sent

--- src/network/Client.java
+++ src/network/Client.java
@@ -5,6 +5,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.AbstractMap.SimpleEntry;
 
 import aes.Decryption;
 import aes.Encryption;
@@ -21,18 +25,24 @@ public class Client {
 	String host;
 	String name;
 	int serverPort;
-	
+
+	private ArrayList<ArrayList<String>> chats = new ArrayList<>();
+
+	public static final String[] encryptions = new String[] { "aes" };
+
 //	public CopyOnWriteArrayList<String> msgQueue = new CopyOnWriteArrayList<>();
 
 	private boolean loop = true;
 	private int sendAttempts = 0;
 
-	public boolean isWaiting() {return loop;}
-	
+	public boolean isWaiting() {
+		return loop;
+	}
+
 	public void registerToServer(String host, int port, String name) {
 		this.host = host;
 		this.serverPort = port;
-		
+
 		try {
 			client = new Socket(host, port);
 			out = new DataOutputStream(client.getOutputStream());
@@ -51,8 +61,8 @@ public class Client {
 	public void send(String s) {
 		if (s.equals("q")) {
 			deRegister();
-		}else if (s.startsWith("me")) {// wants to encrypt message
-			
+		} else if (s.startsWith("me")) {// wants to encrypt message
+
 			String msg = s.substring(2);
 			String name;
 
@@ -71,13 +81,13 @@ public class Client {
 				return;
 			}
 
-			if (msg.startsWith(" ")) {// aes as standard encryption: message is altered to specific aes encryption request
+			if (msg.startsWith(" ")) {// aes as standard encryption: message is altered to specific aes encryption
+										// request
 				msg = "aes" + msg.substring(1);
 			}
 			if (msg.startsWith("aes")) {// specific request to encrypt with aes
 				msg = msg.substring(3);
 
-				
 				// encrypting the main message
 				String encrypted = Encryption.encrypt(msg);
 				String key = KeySchedule.Key;
@@ -90,26 +100,25 @@ public class Client {
 				for (int i = 0; i < 8; i++) {
 					keyAschars[i] = (char) Integer.parseInt(splitKey[i], 2);
 				}
-				
+
 				// preparing message head
 				String send = "me" + name + "\\aes";
-				
+
 				// adding key
 				for (char c : keyAschars) {
 					send += "" + c;
 				}
-				
+
 				// adding encrypted message
 				send += encrypted;
-				
-				
-				if(out == null){// OutputStream is not yet defined
+
+				if (out == null) {// OutputStream is not yet defined
 					try {
 						out = new DataOutputStream(client.getOutputStream());
 					} catch (IOException e) {
 						System.err.println("I/O error: could not get DataOutputStream:");
 						e.printStackTrace();
-						if(sendAttempts++ > 10) {
+						if (sendAttempts++ > 10) {
 							System.err.println("problem constantly recurring...\nshutdown initiated...");
 							deRegister();
 							return;
@@ -121,9 +130,9 @@ public class Client {
 						return;
 					}
 				}
-				
+
 				sendAttempts = 0;
-				
+
 				try {// sending message to Server
 					out.writeUTF(send);
 				} catch (IOException e) {
@@ -132,7 +141,7 @@ public class Client {
 					return;
 				}
 			}
-		}else {
+		} else {
 
 			try {
 				out.writeUTF(s);
@@ -152,51 +161,54 @@ public class Client {
 		}
 
 		while (loop) {
-			
+
 			try {
 				String s = in.readUTF();
 				if (s.startsWith(">c")) {
 					if (s.length() > 3) {
 						s = s.substring(3);
 						contacts = s.split(" ");
-						System.out.println("received updated contacts list:\n\t" + s.replace(" ", "\n\t").replace(name, "").replace("\n\n", "\n"));
+						System.out.println("received updated contacts list:\n\t"
+								+ s.replace(" ", "\n\t").replace(name, "").replace("\n\n", "\n"));
 					}
 				} else if (s.startsWith("n")) {
 					name = s.substring(1);
 					System.out.println("name changed to: \"" + name + "\"");
-				}else if (s.startsWith("me")) {
+				} else if (s.startsWith("me")) {
 					String origin = s.substring(2, s.indexOf('\\'));
-					String encrMsg = s.substring(s.indexOf('\\')+12);
-					char[] keyAsChars = s.substring(s.indexOf('\\')+4, s.indexOf('\\')+12).toCharArray();
-					
+					String encrMsg = s.substring(s.indexOf('\\') + 12);
+					char[] keyAsChars = s.substring(s.indexOf('\\') + 4, s.indexOf('\\') + 12).toCharArray();
+
 					String keyBinStr = "";
-					
+
 					for (char c : keyAsChars) {
-						String t = Integer.toBinaryString((int)c);
-						while(t.length() < 16) t = "0" + t;
+						String t = Integer.toBinaryString((int) c);
+						while (t.length() < 16)
+							t = "0" + t;
 						keyBinStr += t;
 					}
-					
+
 					String msg = Decryption.decrypt(encrMsg, keyBinStr);
-					
+
 					System.out.println("Message by '" + origin + "': \n" + msg);
-					
-				}else if(s.startsWith("mp")) {
-					if(s.contains("\\") && !s.substring(2).startsWith("\\")) {
+
+				} else if (s.startsWith("mp")) {
+					if (s.contains("\\") && !s.substring(2).startsWith("\\")) {
 						String name = s.substring(2, s.indexOf('\\'));
-						System.out.println("message from <" + name + ">\n\t" + s.substring(s.indexOf('\\')+1));
-					}else {
+						System.out.println("message from <" + name + ">\n\t" + s.substring(s.indexOf('\\') + 1));
+					} else {
 						s = s.substring(2);
-						if(s.startsWith("\\")) {
+						if (s.startsWith("\\")) {
 							s = s.substring(1);
 						}
 						System.out.println("Message from <Anonymus>:\n\t" + s);
 					}
 				}
 			} catch (IOException e) {
-				if (loop)e.printStackTrace();
+				if (loop)
+					e.printStackTrace();
 			}
-			
+
 		}
 	}
 
@@ -209,7 +221,7 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void reboot() {
 		deRegister();
 		try {
@@ -220,19 +232,78 @@ public class Client {
 		registerToServer(host, serverPort, name);
 	}
 
-	public static class ClientThread extends Thread{
-		
+	public void beginChat(String name, String encryption) {
+		chats.add(new ArrayList<String>());
+		chats.get(chats.size() - 1).add(name);
+		chats.get(chats.size() - 1).add(encryption);
+	}
+
+	public void beginChat(String name) {
+		chats.add(new ArrayList<String>());
+		chats.get(chats.size() - 1).add(name);
+		chats.get(chats.size() - 1).add("");
+	}
+
+	public void encryptChat(String name, String encryption) {
+		for (ArrayList<String> chat : chats) {
+			if (chat.get(0).equals(name)) {
+				chat.set(1, encryption);
+			}
+		}
+	}
+
+	public boolean sendThroughChat(String name, String msg) {
+		ArrayList<String> chat = null;
+		for (ArrayList<String> c : chats) {
+			if (c.get(0).equals(name)) {
+				chat = c;
+			}
+		}
+
+		if (chat == null) {
+			System.err.println("Error: could not find current chat with '" + name + "'");
+			return false;
+		}
+
+		String s = "mp";
+
+		if (chat.get(1).equals("")) {
+			s = "me";
+		}
+
+		s += name + "\\" + chat.get(1) + msg;
+
+		chat.add(msg);
+
+		return true;
+	}
+
+	public String[][] getChats() {
+		String[][] ret = new String[chats.size()][];
+
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = new String[chats.get(i).size()];
+			for (int j = 0; j < ret[i].length; j++) {
+				ret[i][j] = chats.get(i).get(j);
+			}
+		}
+
+		return ret;
+	}
+
+	public static class ClientThread extends Thread {
+
 		private Client client;
-		
+
 		public ClientThread(Client client) {
 			this.client = client;
 		}
-		
+
 		@Override
 		public void run() {
-			while(client.loop)client.waitForMessage();
+			while (client.loop)
+				client.waitForMessage();
 		}
 	}
 
 }
-
